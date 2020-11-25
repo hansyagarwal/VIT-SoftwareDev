badab99fd071d72476d27e3de2de87b0f0bc090e
ARYAN AGARWAL <hansy.agarwal@gmail.com>
2020-11-25 21:11:43 +0530

    basic game

--- public/js/pve.js
+++ public/js/pve.js
@@ -1,1 +1,18 @@
-console.log("hello")
+let  scor = 0;
+let count = 0
+let ballcount = 0;
+let ball = document.querySelector("#ball").textContent;
+document.querySelector("#hit").addEventListener("click",()=>{
+    const s = Math.floor(Math.random() * 6) + 1;
+    scor = scor + s;
+    ball = ball - 1;
+    document.querySelector("#ball").innerHTML = ball
+    document.querySelector("#score").innerHTML = scor;
+    document.querySelector("#caption").innerHTML = s;
+    count++;
+    if(count==6)
+    {
+        document.querySelector("#hit").disabled = true;
+    }
+})
+
