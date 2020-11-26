e70912315300f8f08147cedf8e70aaf77a4be1fb
ARYAN AGARWAL <hansy.agarwal@gmail.com>
2020-11-26 15:55:20 +0530

    pawn movement

--- HitwicketQues.java
+++ HitwicketQues.java
@@ -9,8 +9,8 @@
  * @author hansy
  */
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+//import java.lang.*;
+//import java.io.*;
 
 public class HitwicketQues {
 
@@ -33,7 +33,79 @@ public class HitwicketQues {
             String player = sc.next();
             grid[0][i] = player;   
         }
-        System.out.println(grid[4][4]);
+        String input="";
+        //while(input!="quit")
+        {
+            System.out.println("enter pawn number and direction:");
+            input = sc.next();
+            String direction  = sc.next();
+            if(direction == "F")
+            {
+                for(int i=0;i<5;i++)
+                {
+                    for(int j=0;j<5;j++)
+                    {
+                        if(grid[i][j] == input)
+                        {
+                            grid[i][j] = "0";
+                            grid[i][j+1] = input;
+                        }
+                    }
+                }
+            }
+            if(direction == "B")
+            {
+                for(int i=0;i<5;i++)
+                {
+                    for(int j=0;j<5;j++)
+                    {
+                        if(grid[i][j] == input)
+                        {
+                            grid[i][j] = "0";
+                            grid[i][j-1] = input;
+                        }
+                    }
+                }
+            }
+            if(direction == "L")
+            {
+                for(int i=0;i<5;i++)
+                {
+                    for(int j=0;j<5;j++)
+                    {
+                        if(grid[i][j] == input)
+                        {
+                            grid[i][j] = "0";
+                            grid[i-1][j] = input;
+                        }
+                    }
+                }
+            }
+            if(direction == "R")
+            {
+                for(int i=0;i<5;i++)
+                {
+                    for(int j=0;j<5;j++)
+                    {
+                        if(grid[i][j] == input)
+                        {
+                            grid[i][j] = "0";
+                            grid[i+1][j] = input;
+                        }
+                    }
+                }
+            }
+
+        }
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        //System.out.println(grid[4][4]);
     }
     
 }
